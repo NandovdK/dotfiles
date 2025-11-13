39c98d72031186944d6bf17e85b47086d8406e90
NandovdK <njvdkant@gmail.com>
2025-11-13 22:38:18 +0100

    some changes to make wezterm usable on my windows machine

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,31 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+function getOS()
+	local sep = package.config:sub(1, 1)
+
+	if sep == "\\" then
+		return "Windows"
+	end
+
+	local handle = io.popen("uname")
+	local result = handle:read("*a")
+
+	handle:close()
+	result = result:lower()
+
+	if result:find("linux") then
+		return "Linux"
+	end
+	return "macOS"
+end
+
+local os_name = getOS()
+
 config.color_scheme = "Batman"
 config.font = wezterm.font("JetBrains Mono")
 config.window_padding = {
-	left = 0,
+	left = 3,
 	right = 3,
 	top = 3,
 	bottom = 0,
@@ -12,7 +33,7 @@ config.window_padding = {
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = false
-config.show_new_tab_button_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = true
 
 config.window_decorations = "RESIZE"
 -- config.window_background_opacity = 0.9
@@ -20,5 +41,11 @@ config.window_decorations = "RESIZE"
 config.cursor_blink_rate = 800
 config.default_cursor_style = "SteadyBlock"
 config.max_fps = 120
+
+-- windows overwrites
+if os_name == "Windows" then
+	config.default_domain = "WSL:Ubuntu"
+	config.hide_tab_bar_if_only_one_tab = false
+end
 
 return config
